dfecd0a9171f240d7b0edbd853739c04096d1201
Pedro Monteiro <montteiropedro@gmail.com>
2023-08-02 17:51:37 -0300

    WIP:POST /api/v1/ads, e corrigido banco de dados de teste
    Co-authored-by: MatheusOB21 <matheus53barros@hotmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::ActiveRecordError, with: :internal_server_error
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
   rescue_from ActiveRecord::RecordInvalid, with: :invalid
+  rescue_from ArgumentError, with: :bad_request
 
   private
 
@@ -13,7 +14,14 @@ class ApplicationController < ActionController::API
     render status: :not_found
   end
 
+  def bad_request(err)
+    message = err.message.gsub(/('.*')/, '').split
+    errors = {}
+    errors[message.pop] = message.join(' ')
+    render status: :bad_request, json: { message: 'Erro nos parâmetros enviados', errors: }
+  end
+
   def invalid(error)
-    render status: :unprocessable_entity, json: { errors: error.record.errors.full_messages }
+    render status: :unprocessable_entity, json: { errors: error.record.errors }
   end
 end
---
+++ spec/requests/create_ad_api_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe 'API para criação de anúncios' do
+  context 'POST /api/v1/ads' do
+    it 'com sucesso' do
+      post '/api/v1/ads', params: { ad: { game_mode: :unranked, platform: :battle_net,
+                                          platform_indentification: 'PlayerNumberOne',
+                                          years_playing: 7, favorite_classes: :tank,
+                                          hour_start: 360, hour_end: 480, voice_channel: true } }
+
+      expect(response).to have_http_status :created
+      expect(response.body).to include 'unranked'
+      expect(response.body).not_to include 'competitive'
+      expect(response.body).to include 'battle_net'
+      expect(response.body).not_to include 'steam'
+      expect(response.body).to include 'PlayerNumberOne'
+      expect(response.body).to include '7'
+      expect(response.body).to include 'tank'
+      expect(response.body).to include '360'
+      expect(response.body).to include '480'
+    end
+    it 'sem sucesso, pois está faltando parâmetros' do
+      post '/api/v1/ads', params: { ad: { platform: :battle_net,
+                                          platform_indentification: 'PlayerNumberOne',
+                                          years_playing: 7, favorite_classes: :tank,
+                                          hour_start: 360, hour_end: 480, voice_channel: true } }
+
+      expect(response).to have_http_status 422
+      res_body = response.parsed_body
+      expect(res_body['errors']['game_mode']).to include 'Não pode ficar branco'
+    end
+  end
+end
