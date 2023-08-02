d01dbc1da7e137332fd60a67b81dc94775f96484
Pedro Monteiro <montteiropedro@gmail.com>
2023-08-02 14:45:17 -0300

    Modificações no api_controller e fix ofensas rubocop
    Co-authored-by: MatheusOB21 <matheus53barros@hotmail.com>

---
+++ app/controllers/api/v1/ads_controller.rb
@@ -0,0 +1,3 @@
+class Api::V1::AdsController < Api::V1::ApiController
+
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,19 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::ActiveRecordError, with: :internal_server_error
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid
 
   private
 
   def internal_server_error
-    render status: 500
+    render status: :internal_server_error
   end
 
   def not_found
-    render status: 404
+    render status: :not_found
+  end
+
+  def invalid(error)
+    render status: :unprocessable_entity, json: { errors: error.record.errors.full_messages }
   end
 end
